2fcf13972fe3186ba4b3fa592e1b720f86262623
Shravan Kumar Moduga <shravan3001@hotmail.com>
2026-09-02 09:38:12 +0530

    [3875][easy] construct union parity array I

---
+++ easy/3875_construct_union_parity_array_I.py
@@ -0,0 +1,60 @@
+"""
+https://leetcode.com/problems/construct-uniform-parity-array-i/description/?envType=daily-question&envId=2026-09-02
+3875. Construct Uniform Parity Array I
+
+You are given an array nums1 of n distinct integers.
+You want to construct another array nums2 of length n such that the elements in nums2 are either all odd or all even.
+For each index i, you must choose exactly one of the following (in any order):
+    nums2[i] = nums1[i]
+    nums2[i] = nums1[i] - nums1[j], for an index j != i
+Return true if it is possible to construct such an array, otherwise, return false.
+
+Example 1:
+Input: nums1 = [2,3]
+Output: true
+Explanation:
+    Choose nums2[0] = nums1[0] - nums1[1] = 2 - 3 = -1.
+    Choose nums2[1] = nums1[1] = 3.
+    nums2 = [-1, 3], and both elements are odd. Thus, the answer is true
+
+Example 2:
+Input: nums1 = [4,6]
+Output: true
+Explanation:
+    Choose nums2[0] = nums1[0] = 4.
+    Choose nums2[1] = nums1[1] = 6.
+    nums2 = [4, 6], and all elements are even. Thus, the answer is true.
+
+Constraints:
+
+    1 <= n == nums1.length <= 100
+    1 <= nums1[i] <= 100
+    nums1 consists of distinct integers.
+"""
+
+import unittest
+
+
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        # even - odd, odd - even are odd, can convert evens to odds
+        return True
+
+
+class TestUniformArray(unittest.TestCase):
+    def setUp(self) -> None:
+        self.sol = Solution()
+
+    def test_example_1(self):
+        nums = [2, 3]
+        output = True
+        self.assertEqual(self.sol.uniformArray(nums), output)
+
+    def test_example_2(self):
+        nums = [4, 6]
+        output = True
+        self.assertEqual(self.sol.uniformArray(nums), output)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
