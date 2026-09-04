a13e940efa38dd31f5a2020d4f1802c98c94d360
Shravan Kumar Moduga <shravan3001@hotmail.com>
2026-09-04 11:29:55 +0530

    [3903][easy] smallest stable index

---
+++ easy/3903_smallest_stable_index.py
@@ -0,0 +1,88 @@
+"""
+https://leetcode.com/problems/smallest-stable-index-i/description/?envType=daily-question&envId=2026-09-04
+3903. Smallest Stable Index I
+
+You are given an integer array nums of length n and an integer k.
+For each index i, define its instability score as max(nums[0..i]) - min(nums[i..n - 1]).
+In other words:
+    max(nums[0..i]) is the largest value among the elements from index 0 to index i.
+    min(nums[i..n - 1]) is the smallest value among the elements from index i to index n - 1.
+An index i is called stable if its instability score is less than or equal to k.
+Return the smallest stable index. If no such index exists, return -1.
+
+Example 1:
+Input: nums = [5,0,1,4], k = 3
+Output: 3
+Explanation:
+    At index 0: The maximum in [5] is 5, and the minimum in [5, 0, 1, 4] is 0, so the instability score is 5 - 0 = 5.
+    At index 1: The maximum in [5, 0] is 5, and the minimum in [0, 1, 4] is 0, so the instability score is 5 - 0 = 5.
+    At index 2: The maximum in [5, 0, 1] is 5, and the minimum in [1, 4] is 1, so the instability score is 5 - 1 = 4.
+    At index 3: The maximum in [5, 0, 1, 4] is 5, and the minimum in [4] is 4, so the instability score is 5 - 4 = 1.
+    This is the first index with an instability score less than or equal to k = 3. Thus, the answer is 3.
+
+Example 2:
+Input: nums = [3,2,1], k = 1
+Output: -1
+Explanation:
+    At index 0, the instability score is 3 - 1 = 2.
+    At index 1, the instability score is 3 - 1 = 2.
+    At index 2, the instability score is 3 - 1 = 2.
+    None of these values is less than or equal to k = 1, so the answer is -1.
+""
+Example 3:
+Input: nums = [0], k = 0
+Output: 0
+Explanation:
+At index 0, the instability score is 0 - 0 = 0, which is less than or equal to k = 0. Therefore, the answer is 0.
+
+Constraints:
+
+    1 <= nums.length <= 100
+    0 <= nums[i] <= 109
+    0 <= k <= 109
+"""
+
+import unittest
+
+
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+        postMin = [0] * n
+        postMin[-1] = nums[-1]
+        for i in range(n - 2, -1, -1):
+            postMin[i] = nums[i] if nums[i] < postMin[i + 1] else postMin[i + 1]
+
+        preMax = float("-inf")
+        for i, x in enumerate(nums):
+            preMax = x if x > preMax else preMax
+            if preMax - postMin[i] <= k:
+                return i
+        return -1
+
+
+class TestFirstStableIndex(unittest.TestCase):
+    def setUp(self) -> None:
+        self.sol = Solution()
+
+    def test_example_1(self):
+        nums = [5, 0, 1, 4]
+        k = 3
+        output = 3
+        self.assertEqual(self.sol.firstStableIndex(nums, k), output)
+
+    def test_example_2(self):
+        nums = [3, 2, 1]
+        k = 1
+        output = -1
+        self.assertEqual(self.sol.firstStableIndex(nums, k), output)
+
+    def test_example_3(self):
+        nums = [0]
+        k = 0
+        output = 0
+        self.assertEqual(self.sol.firstStableIndex(nums, k), output)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
